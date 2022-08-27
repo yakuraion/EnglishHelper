9963553d75a6eb979fb4c6bc0ce5f307ac3db669
Vyacheslav Yakovlev <yakuraion@gmail.com>
2022-08-27 17:40:52 +0300

    add vocabulary module

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -17,6 +17,7 @@ dependencyResolutionManagement {
 rootProject.name = "EnglishHelper"
 
 include(":app")
+include(":modules:vocabulary")
 
 includeBuild("plugins/code-check") {
     dependencySubstitution {
