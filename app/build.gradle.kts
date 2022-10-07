9b72975873727cc3b9f63018306802e522b36e21
Vyacheslav Yakovlev <yakuraion@gmail.com>
2022-10-07 23:45:05 +0300

    add signing

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,6 @@
+import java.io.FileInputStream
+import java.util.*
+
 plugins {
     id("com.android.application")
     id("base-convention")
@@ -12,9 +15,32 @@ android {
         versionName = "0.1.0"
     }
 
+    val prodKeystorePropertiesFile = file(
+        "${System.getProperty("user.home")}/keystores/EnglishHelper/credentials.properties"
+    )
+    val prodKeystoreProperties = Properties().apply { load(FileInputStream(prodKeystorePropertiesFile)) }
+    signingConfigs {
+        create("prod") {
+            keyAlias = prodKeystoreProperties.getProperty("keyAlias")
+            keyPassword = prodKeystoreProperties.getProperty("keyPassword")
+            storeFile = file(prodKeystoreProperties.getProperty("keystorePath"))
+            storePassword = prodKeystoreProperties.getProperty("keystorePassword")
+        }
+        create("dev") {
+            keyAlias = "dev"
+            keyPassword = "password"
+            storeFile = file("dev-keystore.jks")
+            storePassword = "password"
+        }
+    }
+
     buildTypes {
+        getByName("release") {
+            signingConfig = signingConfigs.getByName("prod")
+        }
         getByName("debug") {
             applicationIdSuffix = ".debug"
+            signingConfig = signingConfigs.getByName("dev")
         }
     }
 }
