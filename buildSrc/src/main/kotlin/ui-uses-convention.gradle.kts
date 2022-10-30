7513cd784bebbb7de12f7c75856e4839653893ef
Vyacheslav Yakovlev <yakuraion@gmail.com>
2022-10-30 22:36:22 +0300

    add navigation dependencies

--- buildSrc/src/main/kotlin/ui-uses-convention.gradle.kts
+++ buildSrc/src/main/kotlin/ui-uses-convention.gradle.kts
@@ -17,20 +17,23 @@ configure<BaseExtension> {
 dependencies {
     add("implementation", versionCatalog.findLibrary("androidx-core-ktx").get())
     add("implementation", versionCatalog.findLibrary("android-lifecycle").get())
+    add("androidTestImplementation", versionCatalog.findLibrary("androidx-junit").get())
 
     add("implementation", versionCatalog.findLibrary("compose-material-icons-extended").get())
     add("implementation", versionCatalog.findLibrary("compose-material3").get())
     add("implementation", versionCatalog.findLibrary("compose-tooling-preview").get())
-
     add("implementation", versionCatalog.findLibrary("compose-activity").get())
     add("implementation", versionCatalog.findLibrary("compose-viewmodel").get())
-
     add("debugImplementation", versionCatalog.findLibrary("compose-tooling").get())
+    add("androidTestImplementation", versionCatalog.findLibrary("compose-junit").get())
+
+    add("implementation", versionCatalog.findLibrary("navigation-fragment").get())
+    add("implementation", versionCatalog.findLibrary("navigation-ui").get())
+    add("implementation", versionCatalog.findLibrary("navigation-compose").get())
+    add("debugImplementation", versionCatalog.findLibrary("navigation-testing").get())
 
     add("debugImplementation", versionCatalog.findLibrary("custom-view").get())
     add("debugImplementation", versionCatalog.findLibrary("custom-view-pooling-container").get())
 
-    add("androidTestImplementation", versionCatalog.findLibrary("androidx-junit").get())
-    add("androidTestImplementation", versionCatalog.findLibrary("compose-junit").get())
     add("androidTestImplementation", versionCatalog.findLibrary("espresso").get())
 }
