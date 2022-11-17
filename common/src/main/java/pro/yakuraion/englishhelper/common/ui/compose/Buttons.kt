82da9e1d3f9238cf185984ce1384e659b538e8f4
Vyacheslav Yakovlev <yakuraion@gmail.com>
2022-11-18 01:45:11 +0300

    add count for learning words to OverviewScreen

--- common/src/main/java/pro/yakuraion/englishhelper/common/ui/compose/Buttons.kt
+++ common/src/main/java/pro/yakuraion/englishhelper/common/ui/compose/Buttons.kt
@@ -2,6 +2,7 @@ package pro.yakuraion.englishhelper.common.ui.compose
 
 import android.content.res.Configuration
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Start
@@ -9,10 +10,12 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import pro.yakuraion.englishhelper.common.ui.theme.AppTheme
@@ -21,10 +24,12 @@ import pro.yakuraion.englishhelper.common.ui.theme.AppTheme
 fun FloatingButtonWithIconAndText(
     icon: ImageVector,
     text: String,
-    onClick: () -> Unit
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier
 ) {
     Button(
         onClick = { onClick() },
+        modifier = modifier,
         colors = ButtonDefaults.buttonColors(MaterialTheme.colorScheme.tertiaryContainer)
     ) {
         Icon(
@@ -35,7 +40,9 @@ fun FloatingButtonWithIconAndText(
         Spacer(modifier = Modifier.width(8.dp))
         Text(
             text = text,
-            color = MaterialTheme.colorScheme.onTertiaryContainer
+            color = MaterialTheme.colorScheme.onTertiaryContainer,
+            fontWeight = FontWeight.Medium,
+            style = MaterialTheme.typography.titleMedium
         )
     }
 }
@@ -50,5 +57,35 @@ fun FloatingButtonWithIconAndTextPreview() {
             text = "Start",
             onClick = {}
         )
+    }
+}
+
+@Composable
+fun InContentFullWidthButton(
+    text: String,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    OutlinedButton(
+        onClick = onClick,
+        modifier = modifier.fillMaxWidth(),
+        shape = MaterialTheme.shapes.extraSmall,
+        colors = ButtonDefaults.outlinedButtonColors(contentColor = MaterialTheme.colorScheme.onBackground)
+    ) {
+        Text(
+            text = text,
+            color = MaterialTheme.colorScheme.onBackground,
+            fontWeight = FontWeight.Medium,
+            style = MaterialTheme.typography.bodyLarge
+        )
+    }
+}
+
+@Preview
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Composable
+fun InContentButtonPreview() {
+    AppTheme {
+        InContentFullWidthButton(text = "Push me", onClick = {})
     }
 }
--- modules/vocabulary/src/main/java/pro/yakuraion/englishhelper/vocabulary/ui/overview/OverviewScreen.kt
+++ modules/vocabulary/src/main/java/pro/yakuraion/englishhelper/vocabulary/ui/overview/OverviewScreen.kt
@@ -7,11 +7,14 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.PlayArrow
 import androidx.compose.material3.FabPosition
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import pro.yakuraion.englishhelper.common.ui.compose.FloatingButtonWithIconAndText
+import pro.yakuraion.englishhelper.common.ui.compose.InContentFullWidthButton
 import pro.yakuraion.englishhelper.common.ui.theme.AppTheme
 import pro.yakuraion.englishhelper.vocabulary.R
 
@@ -21,18 +24,33 @@ fun OverviewScreen(
     onStartTestingClick: () -> Unit
 ) {
     Scaffold(
-        floatingActionButton = {
-            FloatingButtonWithIconAndText(
-                icon = Icons.Filled.PlayArrow,
-                text = stringResource(id = R.string.vocabulary_overview_screen_start_testing),
-                onClick = { onStartTestingClick() }
-            )
-        },
+        floatingActionButton = { StartTestingButton(onStartTestingClick = onStartTestingClick) },
         floatingActionButtonPosition = FabPosition.Center
     ) { paddingValues ->
         Column(modifier = Modifier.padding(paddingValues)) {
-
+            LearningWordsView(wordsCount = 10, onAddMoreClick = onAddWordsClick)
         }
+    }
+}
+
+@Composable
+fun StartTestingButton(onStartTestingClick: () -> Unit) {
+    FloatingButtonWithIconAndText(
+        icon = Icons.Filled.PlayArrow,
+        text = stringResource(R.string.vocabulary_overview_screen_start_testing),
+        onClick = onStartTestingClick
+    )
+}
+
+@Composable
+fun LearningWordsView(wordsCount: Int, onAddMoreClick: () -> Unit) {
+    Column {
+        Text(text = stringResource(R.string.vocabulary_overview_screen_current_learning_words, wordsCount))
+        InContentFullWidthButton(
+            text = stringResource(R.string.vocabulary_overview_screen_add_words),
+            onClick = onAddMoreClick,
+            modifier = Modifier.padding(horizontal = 16.dp)
+        )
     }
 }
 
