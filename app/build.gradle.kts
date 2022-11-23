0dc8d3773dd0b8c50b1d3d6bcf8a12a4b73e90c4
Vyacheslav Yakovlev <yakuraion@gmail.com>
2022-11-24 00:16:52 +0300

    update versionName to 0.2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
 
     defaultConfig {
         applicationId = "pro.yakuraion.englishhelper"
-        versionCode = 1
-        versionName = "0.1.0"
+        versionCode = 2
+        versionName = "0.2.0"
     }
 
     val releaseKeystorePropertiesFile = file(
