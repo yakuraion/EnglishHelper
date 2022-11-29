4a662d65dcbe6e8b84af31c55e461ac220617af3
Vyacheslav Yakovlev <yakuraion@gmail.com>
2022-11-29 23:05:24 +0300

    refactor TestingWordSimple.kt

--- modules/vocabulary/src/main/java/pro/yakuraion/englishhelper/vocabulary/ui/testing/TestingWordSimple.kt
+++ modules/vocabulary/src/main/java/pro/yakuraion/englishhelper/vocabulary/ui/testing/TestingWordSimple.kt
@@ -9,7 +9,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -22,6 +21,7 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import pro.yakuraion.englishhelper.commonui.compose.theme.AppTheme
+import pro.yakuraion.englishhelper.commonui.compose.widgets.buttons.AppButtonWithText
 import pro.yakuraion.englishhelper.vocabulary.R
 
 @Composable
@@ -31,45 +31,69 @@ fun TestingWordSimple(
 ) {
     Box(modifier = Modifier.fillMaxSize()) {
         Column(modifier = Modifier.align(Alignment.Center)) {
-            Text(
-                text = stringResource(id = R.string.vocabulary_testing_screen_simple_question),
-                modifier = Modifier.fillMaxWidth(),
-                textAlign = TextAlign.Center,
-                style = MaterialTheme.typography.titleMedium
+            val spaceHeight = 16.dp
+            TitleText()
+            Spacer(modifier = Modifier.height(spaceHeight))
+            WordText(
+                word = word,
+                modifier = Modifier.align(Alignment.CenterHorizontally)
             )
-            Spacer(modifier = Modifier.height(16.dp))
-            Text(
-                text = word,
-                modifier = Modifier
-                    .align(Alignment.CenterHorizontally)
-                    .background(
-                        color = MaterialTheme.colorScheme.primaryContainer,
-                        shape = MaterialTheme.shapes.large
-                    )
-                    .padding(horizontal = 24.dp, vertical = 8.dp),
-                color = MaterialTheme.colorScheme.onPrimaryContainer,
-                fontWeight = FontWeight.Medium,
-                style = MaterialTheme.typography.headlineLarge
-            )
-            Spacer(modifier = Modifier.height(16.dp))
-            Text(
-                text = stringResource(id = R.string.vocabulary_testing_screen_simple_tip),
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .alpha(0.6f),
-                textAlign = TextAlign.Center,
-                style = MaterialTheme.typography.labelMedium
-            )
+            Spacer(modifier = Modifier.height(spaceHeight))
+            AdditionalText()
         }
-        Button(
+        AppButtonWithText(
+            text = stringResource(id = R.string.vocabulary_testing_screen_simple_yes),
             onClick = onRememberClick,
             modifier = Modifier
                 .align(Alignment.BottomCenter)
                 .padding(bottom = 16.dp)
-        ) {
-            Text(text = stringResource(id = R.string.vocabulary_testing_screen_simple_yes))
-        }
+        )
     }
+}
+
+@Composable
+private fun TitleText(
+    modifier: Modifier = Modifier
+) {
+    Text(
+        text = stringResource(id = R.string.vocabulary_testing_screen_simple_question),
+        modifier = modifier.fillMaxWidth(),
+        textAlign = TextAlign.Center,
+        style = MaterialTheme.typography.titleMedium
+    )
+}
+
+@Composable
+private fun WordText(
+    word: String,
+    modifier: Modifier = Modifier
+) {
+    Text(
+        text = word,
+        modifier = modifier
+            .background(
+                color = MaterialTheme.colorScheme.primaryContainer,
+                shape = MaterialTheme.shapes.large
+            )
+            .padding(horizontal = 24.dp, vertical = 8.dp),
+        color = MaterialTheme.colorScheme.onPrimaryContainer,
+        fontWeight = FontWeight.Medium,
+        style = MaterialTheme.typography.headlineLarge
+    )
+}
+
+@Composable
+private fun AdditionalText(
+    modifier: Modifier = Modifier
+) {
+    Text(
+        text = stringResource(id = R.string.vocabulary_testing_screen_simple_tip),
+        modifier = modifier
+            .fillMaxWidth()
+            .alpha(0.6f),
+        textAlign = TextAlign.Center,
+        style = MaterialTheme.typography.labelMedium
+    )
 }
 
 @Preview
