b286ec31ff5cdda0509fa8b97a8454a47211c387
Vyacheslav Yakovlev <yakuraion@gmail.com>
2022-12-18 02:45:54 +0300

    add fade to testing examples

---
+++ common-ui/src/main/java/pro/yakuraion/englishhelper/commonui/compose/widgets/layout/AppFadingEdgesBox.kt
@@ -0,0 +1,102 @@
+package pro.yakuraion.englishhelper.commonui.compose.widgets.layout
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxScope
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawWithContent
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+// todo: optimize
+@Composable
+fun AppFadingEdgesBox(
+    backgroundColor: Color,
+    modifier: Modifier = Modifier,
+    leftFade: Dp = 0.dp,
+    topFade: Dp = 0.dp,
+    rightFade: Dp = 0.dp,
+    bottomFade: Dp = 0.dp,
+    content: @Composable BoxScope.() -> Unit,
+) {
+    Box(
+        modifier = modifier
+            .drawWithContent {
+                drawContent()
+
+                if (leftFade != 0.dp) {
+                    drawRect(
+                        brush = Brush.horizontalGradient(
+                            colors = listOf(
+                                backgroundColor,
+                                Color.Transparent
+                            ),
+                            endX = leftFade.toPx()
+                        ),
+                        size = size.copy(width = leftFade.toPx())
+                    )
+                }
+
+                if (topFade != 0.dp) {
+                    drawRect(
+                        Brush.verticalGradient(
+                            colors = listOf(
+                                backgroundColor,
+                                Color.Transparent
+                            ),
+                            endY = topFade.toPx()
+                        ),
+                        size = size.copy(height = topFade.toPx())
+                    )
+                }
+
+                if (rightFade != 0.dp) {
+                    drawRect(
+                        Brush.horizontalGradient(
+                            colors = listOf(
+                                Color.Transparent,
+                                backgroundColor,
+                            ),
+                            startX = size.width - rightFade.toPx()
+                        ),
+                        topLeft = Offset(size.width - rightFade.toPx(), 0f),
+                        size = size.copy(width = rightFade.toPx())
+                    )
+                }
+
+                if (bottomFade != 0.dp) {
+                    drawRect(
+                        Brush.verticalGradient(
+                            colors = listOf(
+                                Color.Transparent,
+                                backgroundColor
+                            ),
+                            startY = size.height - bottomFade.toPx()
+                        ),
+                        topLeft = Offset(0f, size.height - bottomFade.toPx()),
+                        size = size.copy(height = bottomFade.toPx())
+                    )
+                }
+            },
+        content = content
+    )
+}
+
+@Preview
+@Composable
+private fun Preview() {
+    Box(modifier = Modifier.background(Color.White)) {
+        AppFadingEdgesBox(
+            backgroundColor = Color.White,
+            bottomFade = 30.dp
+        ) {
+            Text(text = "Some text\n".repeat(5).trim())
+        }
+    }
+}
--- features/vocabulary/src/main/java/pro/yakuraion/englishhelper/vocabulary/ui/testing/states/regular/TestingContentRegularExamplesShowed.kt
+++ features/vocabulary/src/main/java/pro/yakuraion/englishhelper/vocabulary/ui/testing/states/regular/TestingContentRegularExamplesShowed.kt
@@ -1,5 +1,6 @@
 package pro.yakuraion.englishhelper.vocabulary.ui.testing.states.regular
 
+import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -9,12 +10,19 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.min
+import pro.yakuraion.englishhelper.commonui.compose.theme.AppTheme
+import pro.yakuraion.englishhelper.commonui.compose.widgets.layout.AppFadingEdgesBox
 import pro.yakuraion.englishhelper.domain.entities.WordExample
+import pro.yakuraion.englishhelper.vocabulary.ui.testing.states.TestingUiState
 
 @Composable
 fun TestingContentRegularExamplesShowed(
@@ -55,12 +63,25 @@ private fun ExamplesText(
     modifier: Modifier = Modifier
 ) {
     val scrollState = rememberScrollState()
-    Text(
-        text = examples.toText(),
-        modifier = modifier.verticalScroll(scrollState),
-        color = MaterialTheme.colorScheme.secondary,
-        style = MaterialTheme.typography.bodyMedium
-    )
+
+    val scrollMaxOffsetDp = with(LocalDensity.current) { scrollState.maxValue.toDp() }
+    val scrollOffsetInDp = with(LocalDensity.current) { scrollState.value.toDp() }
+
+    val maxFadeSize = 36.dp
+
+    AppFadingEdgesBox(
+        backgroundColor = MaterialTheme.colorScheme.background,
+        modifier = modifier,
+        topFade = min(scrollOffsetInDp, maxFadeSize),
+        bottomFade = min(scrollMaxOffsetDp - scrollOffsetInDp, maxFadeSize)
+    ) {
+        Text(
+            text = examples.toText(),
+            modifier = Modifier.verticalScroll(scrollState),
+            color = MaterialTheme.colorScheme.secondary,
+            style = MaterialTheme.typography.bodyMedium
+        )
+    }
 }
 
 private fun List<WordExample>.toText(): String {
@@ -68,4 +89,31 @@ private fun List<WordExample>.toText(): String {
         .mapIndexed { index, sentence -> "$index. $sentence." }
         .joinToString(separator = "\n")
         .repeat(3)
+}
+
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@Preview
+@Composable
+private fun Preview() {
+    AppTheme {
+        Scaffold {
+            val state = rememberTestingContentRegularState(
+                uiState = TestingUiState.Regular(
+                    queueId = 0,
+                    word = "word",
+                    soundUri = "",
+                    examples = List(10) { index ->
+                        WordExample("Sentence with given %s #$index", "word")
+                    },
+                    dictionaryUrl = ""
+                )
+            )
+            state.onExamplesShowClick()
+
+            TestingContentRegularExamplesShowed(
+                state = state,
+                onVisitedDictionary = {}
+            )
+        }
+    }
 }
