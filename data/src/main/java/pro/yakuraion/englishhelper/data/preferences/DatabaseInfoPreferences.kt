9c3283f913716d9e8c591699a06c97e339ef6df7
Vyacheslav Yakovlev <yakuraion@gmail.com>
2022-12-22 18:55:10 +0300

    fix DatabaseInfoPreferences with initial db version

--- data/src/main/java/pro/yakuraion/englishhelper/data/preferences/DatabaseInfoPreferences.kt
+++ data/src/main/java/pro/yakuraion/englishhelper/data/preferences/DatabaseInfoPreferences.kt
@@ -1,14 +1,18 @@
 package pro.yakuraion.englishhelper.data.preferences
 
 import android.content.SharedPreferences
+import pro.yakuraion.englishhelper.data.database.AppDatabase
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
-internal class DatabaseInfoPreferences @Inject constructor(private val sharedPreferences: SharedPreferences) {
+internal class DatabaseInfoPreferences @Inject constructor(
+    private val sharedPreferences: SharedPreferences,
+    private val database: AppDatabase,
+) {
 
     var lastDatabaseVersion: Int
-        get() = sharedPreferences.getInt(DATABASE_LAST_DATABASE_VERSION, 1)
+        get() = sharedPreferences.getInt(DATABASE_LAST_DATABASE_VERSION, database.openHelper.readableDatabase.version)
         set(value) {
             sharedPreferences.edit().putInt(DATABASE_LAST_DATABASE_VERSION, value).apply()
         }
